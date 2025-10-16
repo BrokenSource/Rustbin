99abafcc03c620c8fb493ab29645e4ab8f7496dd
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-10-16 03:04:57 -0400

    Add the idea of callable shims via python package

--- rustman/__init__.py
+++ rustman/__init__.py
@@ -1,7 +1,29 @@
 import shutil
+import subprocess
+from functools import cache, partial
+from subprocess import CompletedProcess
 
+_which: callable = cache(shutil.which)
 
-# Fixme: Can we assume venv is properly activated?
-def binary() -> str:
-    """Path to the bundled rustup binary"""
-    return shutil.which("rustup-init")
+# Todo: Ensure the venv bin directory is on path
+def shim(*args: str, proxy: str, **kwargs) -> CompletedProcess:
+    return subprocess.run((
+        _which(proxy),
+        *map(str, args),
+    ), **kwargs)
+
+# Export all the shims as python functions
+init          = partial(shim, proxy="rustup-init")
+cargo         = partial(shim, proxy="cargo")
+cargo_clippy  = partial(shim, proxy="cargo-clippy")
+cargo_fmt     = partial(shim, proxy="cargo-fmt")
+cargo_miri    = partial(shim, proxy="cargo-miri")
+clippy_driver = partial(shim, proxy="clippy-driver")
+rls           = partial(shim, proxy="rls")
+rust_analyzer = partial(shim, proxy="rust-analyzer")
+rust_gdb      = partial(shim, proxy="rust-gdb")
+rust_gdbgui   = partial(shim, proxy="rust-gdbgui")
+rust_lldb     = partial(shim, proxy="rust-lldb")
+rustc         = partial(shim, proxy="rustc")
+rustdoc       = partial(shim, proxy="rustdoc")
+rustfmt       = partial(shim, proxy="rustfmt")
